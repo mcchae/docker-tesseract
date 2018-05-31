63d0cfe0fc1bca3d0e330f18d1e9ed6dd6e413bf
MoonChang Chae <mcchae@gmail.com>
2018-05-31 15:42:49 +0900

    [2018년 5월 31일 목요일 15시 42분 49초 KST] from build_docker.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ echo "Do OCR using tesseract V4..."
 for img in $@; do
 	echo -n "$img processing... "
 	AIMG=$(python -c "import os;print(os.path.abspath(\"${img}\"))")
+	#docker run -it --rm -v ${AIMG}:/tmp/ocr.img mcchae/tesseract tesseract /tmp/ocr.img stdout -l kor > ${AIMG}.txt
 	#docker run -it --rm -v ${AIMG}:/tmp/ocr.img mcchae/tesseract tesseract /tmp/ocr.img stdout -l kor+eng -c preserve_interword_spaces=1 > ${AIMG}.txt
-	docker run -it --rm -v ${AIMG}:/tmp/ocr.img mcchae/tesseract tesseract /tmp/ocr.img stdout -l kor > ${AIMG}.txt
+	docker run -it --rm -v ${AIMG}:/tmp/ocr.img mcchae/tesseract tesseract /tmp/ocr.img stdout -l chi_tra+kor > ${AIMG}.txt
 done
 
